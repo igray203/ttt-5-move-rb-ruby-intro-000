c130ae206c6953050e6e6b8d231c46a086324363
ian gray <igrayscale@gmail.com>
2019-08-17 04:19:46 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,15 +1,13 @@
-board =[" "," "," "," "," "," "," "," "," "]
-
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
-
+  
 end
-
-display_board(board)
+board = [" "," "," "," "," "," "," "," "," "]
+puts display_board
 # code your input_to_index and move method here!
 
 def input_to_index(input)
