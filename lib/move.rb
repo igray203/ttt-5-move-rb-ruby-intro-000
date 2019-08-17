f552c5c4737b32eb204277f8fc32743e1bedbd93
ian gray <igrayscale@gmail.com>
2019-08-17 04:37:58 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -19,5 +19,7 @@ def update_array_at_with(array, index, value)
   array[index] = value
 
 end
-
-update_array_at_with(board, 0, "x")
+puts display_board(board)
+fort = gets
+update_array_at_with(board, fort, "x")
+puts display_board(board)
