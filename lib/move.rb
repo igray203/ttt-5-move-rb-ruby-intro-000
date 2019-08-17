9a762f4747b3a42564f1ab55ecef7e0940ae099e
ian gray <igrayscale@gmail.com>
2019-08-17 04:07:33 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,9 +1,9 @@
-def display_board
-  puts " 4 | g | 4 "
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts " 4 | 4 | 4 "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts " 5 | g| 6 "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 board = [" "," "," "," "," "," "," "," "," "]
 puts display_board
